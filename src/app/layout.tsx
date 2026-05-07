a5fd23e3f1b251907a799e6a7d2a55772e9ec13c
kiiiiwi <981845723@qq.com>
2026-05-07 20:36:56 +0800

    fix change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,9 +32,12 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} ${instrumentSerif.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body suppressHydrationWarning className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
